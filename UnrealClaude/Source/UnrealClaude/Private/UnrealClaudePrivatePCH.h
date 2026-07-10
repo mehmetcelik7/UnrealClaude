01449ebd1d3f446517f2c3626d239d7c46452ff4
Mehmet Chehlik <146588916+mehmetcelik7@users.noreply.github.com>
2026-07-10 12:21:13 +0300

    build(mac): include editor types in UE 5.8 PCH

--- UnrealClaude/Source/UnrealClaude/Private/UnrealClaudePrivatePCH.h
+++ UnrealClaude/Source/UnrealClaude/Private/UnrealClaudePrivatePCH.h
@@ -1,6 +1,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UnrealEd.h"
+#include "Engine/Blueprint.h"
+#include "Engine/Engine.h"
+#include "GameFramework/Actor.h"
+#include "Animation/AnimBlueprint.h"
+#include "Animation/AnimMontage.h"
+#include "Animation/AnimSequence.h"
+#include "EdGraph/EdGraph.h"
+#include "EdGraph/EdGraphNode.h"
+#include "EdGraph/EdGraphPin.h"
+#include "Kismet2/BlueprintEditorUtils.h"
+#include "Misc/App.h"
+#include "Misc/EngineVersion.h"
+#include "Misc/PackageName.h"
+#include "Misc/Paths.h"
+#include "Serialization/JsonSerializer.h"
+#include "Serialization/JsonWriter.h"
+#include "Policies/CondensedJsonPrintPolicy.h"
+#include "Widgets/Text/STextBlock.h"
+#include "UObject/UObjectGlobals.h"
+#include "UObject/UnrealType.h"
 
 // Apple Clang 21 diagnoses an intentional first-element JSON lookup that
 // exits a range loop with `break` as unreachable loop increment code. The
